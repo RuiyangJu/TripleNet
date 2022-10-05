756e627d002e1421314936cfff7004d3e3bda1a4
Rui-Yang Ju <102137894+RuiyangJu@users.noreply.github.com>
2022-10-05 19:28:39 +0800

    Add files via upload

---
+++ cifar10/main.py
@@ -0,0 +1,149 @@
+import string
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data
+import torch.backends.cudnn as cudnn
+import torchvision
+from torchvision import transforms as transforms
+import numpy as np
+import argparse
+
+from models import *
+from models.TripleNet import triplenet
+from misc import progress_bar
+
+CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def main():
+    parser = argparse.ArgumentParser(description="cifar-10 with PyTorch")
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--epoch', default=300, type=int, help='number of epochs tp train for')
+    parser.add_argument('--trainBatchSize', default=64, type=int, help='training batch size')
+    parser.add_argument('--testBatchSize', default=64, type=int, help='testing batch size')
+    parser.add_argument('--cuda', default=torch.cuda.is_available(), type=bool, help='whether cuda is in use')
+    parser.add_argument('--pre-trained', default=True, help="use pre-trained model")
+    args = parser.parse_args()
+
+    solver = Solver(args)
+    solver.run()
+
+
+class Solver(object):
+    def __init__(self, config):
+        self.model = None
+        self.lr = config.lr
+        self.epochs = config.epoch
+        self.train_batch_size = config.trainBatchSize
+        self.test_batch_size = config.testBatchSize
+        self.criterion = None
+        self.optimizer = None
+        self.scheduler = None
+        self.device = None
+        self.cuda = config.cuda
+        self.train_loader = None
+        self.test_loader = None
+
+    def load_data(self):
+        train_transform = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.ToTensor()])
+        test_transform = transforms.Compose([transforms.ToTensor()])
+        train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+        self.train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=self.train_batch_size, shuffle=True)
+        test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+        self.test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=self.test_batch_size, shuffle=False)
+
+    def load_model(self):
+        if self.cuda:
+            self.device = torch.device('cuda')
+            cudnn.benchmark = True
+        else:
+            self.device = torch.device('cpu')
+
+        self.model = triplenet().to(self.device)
+
+        self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.9, weight_decay=1e-4, nesterov=True)
+        self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[150, 225], gamma=0.1)
+        self.criterion = nn.CrossEntropyLoss().to(self.device)
+
+    def train(self):
+        print("train:")
+        self.model.train()
+        train_loss = 0.0
+        train_correct = 0.0
+        total = 0.0
+        all_train_iter_loss = []
+        train_loss_values = []
+
+        for batch_num, (data, target) in enumerate(self.train_loader):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            output = self.model(data)
+            loss = self.criterion(output, target)
+            loss.backward()
+            self.optimizer.step()
+            iter_loss = loss.item()
+            all_train_iter_loss.append(iter_loss)
+            train_loss += iter_loss
+            train_loss_values.append(train_loss / (batch_num + 1))
+
+            prediction = torch.max(output, 1)  # second param "1" represents the dimension to be reduced
+            total += target.size(0)
+
+            # train_correct incremented by one if predicted right
+            train_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+
+            progress_bar(batch_num, len(self.train_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss / (batch_num + 1), 100. * train_correct / total, train_correct, total))
+        return train_loss, train_correct / total
+
+
+    def test(self):
+        print("test:")
+        self.model.eval()
+        test_loss = 0
+        test_correct = 0
+        total = 0
+        all_test_iter_loss = []
+        test_loss_values = []
+
+
+        with torch.no_grad():
+            for batch_num, (data, target) in enumerate(self.test_loader):
+                data, target = data.to(self.device), target.to(self.device)
+                output = self.model(data)
+                loss = self.criterion(output, target)
+                iter_loss = loss.item()
+                all_test_iter_loss.append(iter_loss)
+                test_loss += iter_loss
+                prediction = torch.max(output, 1)
+                total += target.size(0)
+                test_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+                test_loss_values.append(test_loss / (batch_num + 1))
+
+                progress_bar(batch_num, len(self.test_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss / (batch_num + 1), 100. * test_correct / total, test_correct, total))
+        return test_loss, test_correct / total
+
+    def save(self):
+        model_out_path = "model.pth"
+        torch.save(self.model.state_dict(), model_out_path)
+        print("Checkpoint saved to {}".format(model_out_path))
+
+    def run(self):
+        self.load_data()
+        self.load_model()
+        accuracy = 0
+        for epoch in range(1, self.epochs + 1):
+            self.scheduler.step(epoch)
+            print("\n===> epoch: %d/300" % epoch)
+            train_result = self.train()
+            print(train_result)
+            test_result = self.test()
+            accuracy = max(accuracy, test_result[1])
+            if epoch == self.epochs:
+                print("===> BEST ACC. PERFORMANCE: %.3f%%" % (accuracy * 100))
+                self.save()
+
+
+if __name__ == '__main__':
+    main()
